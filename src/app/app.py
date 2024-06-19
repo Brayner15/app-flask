2b46f4fff89466a2a849c2edfc014ea419d1f639
Brayner Stiven Cardenas Meneses <braynerstivencardenasmeneses@Air-de-Brayner.bbrouter>
2024-06-19 03:20:04 -0500

    FIX-model

--- src/app/app.py
+++ src/app/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, render_template
 import joblib
-import numpy as np
 import logging
 
 app = Flask(__name__)
@@ -10,8 +9,8 @@ logging.basicConfig(level=logging.DEBUG)
 
 # Cargar el modelo y el vectorizador de sentimiento
 try:
-    model = joblib.load('/code/app/sentiment_model.pkl')
-    vectorizer = joblib.load('/code/app/vectorizer.pkl')
+    model = joblib.load('/code/app/sentiment_model.joblib')
+    vectorizer = joblib.load('/code/app/vectorizer.joblib')
     app.logger.info("Model and vectorizer loaded successfully.")
 except Exception as e:
     app.logger.error("Error loading the model or vectorizer:")
@@ -23,7 +22,6 @@ def index():
         text = request.form["text"]
         prueba = [text]  # Convertir el texto de entrada en una lista de un solo elemento
         try:
-            print(prueba)
             # Transformar el texto usando el vectorizador
             text_transformed = vectorizer.transform(prueba)
             # Realizar la predicción
@@ -31,11 +29,6 @@ def index():
             # Obtener las clases del modelo
             clases = model.classes_
             # Asignar la etiqueta adecuada
-            app.logger.info(f"Text: {text}")
-            app.logger.info(f"Transformed Text: {text_transformed}")
-            app.logger.info(f"Prediction: {P}")
-            app.logger.info(f"Classes: {clases}")
-            print(clases[P[0]])
             if clases[P[0]] == 0:
                 prediction_label = "Negativo"
             elif clases[P[0]] == 2:
@@ -51,4 +44,4 @@ def index():
     return render_template("index.html", text="", prediction="")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080)
